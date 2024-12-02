136b5171eea93c83037fe866891ac7527cdd117d
singlepigeon <adamroyal232@yahoo.com>
2024-12-02 22:01:17 +0000

    Day 2

---
+++ Day2Part1/Program.cs
@@ -0,0 +1,19 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var safeCount = lines
+    .Select(x => x.Split(' ').Select(int.Parse).ToList())
+    .Count(IsSafe);
+
+Console.WriteLine(safeCount);
+
+bool IsSafe(List<int> levels)
+{
+    for (var i = 1; i < levels.Count; i++)
+    {
+        // range check
+        if (Math.Abs(levels[i - 1] - levels[i]) is < 1 or > 3) return false;
+    }
+
+    // check it's equal if ordering 
+    return levels.SequenceEqual(levels.OrderBy((x) => x)) || levels.SequenceEqual(levels.OrderByDescending((x) => x));
+}
---
+++ Day2Part2/Program.cs
@@ -0,0 +1,25 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var parsed = lines
+    .Select(x => x.Split(' ').Select(int.Parse).ToList()).ToList();
+
+var count = parsed.Count(IsSafe) + parsed.Count(x => !IsSafe(x) && SafeInAtLeastOneWay(x));
+
+Console.WriteLine(count);
+
+bool SafeInAtLeastOneWay(List<int> levels)
+{
+    return levels.Where((_, i) => IsSafe(levels.Where((_, j) => j != i).ToList())).Any();
+}
+
+bool IsSafe(List<int> levels)
+{
+    for (var i = 1; i < levels.Count; i++)
+    {
+        // range check
+        if (Math.Abs(levels[i - 1] - levels[i]) is < 1 or > 3) return false;
+    }
+
+    // check it's equal if ordering 
+    return levels.SequenceEqual(levels.OrderBy((x) => x)) || levels.SequenceEqual(levels.OrderByDescending((x) => x));
+}
