a1e55c1561becde06ca05d57ae0d77a6c6e23521
singlepigeon <adamroyal232@yahoo.com>
2024-12-03 22:14:32 +0000

    Day 3

---
+++ Day3Part1/Program.cs
@@ -0,0 +1,92 @@
+﻿using static System.Char;
+
+var input = File.ReadAllText("input.txt");
+
+var expectingChar = 'm';
+var expectingLeftDigit = false;
+var inDigit = false;
+var expectingRightDigit = false;
+var left = 0;
+var right = 0;
+
+var count = input.ToCharArray().Aggregate(0, (current, c) =>
+{
+    if (c == expectingChar)
+    {
+        switch (expectingChar)
+        {
+            case 'm':
+                expectingChar = 'u';
+                break;
+            case 'u':
+                expectingChar = 'l';
+                break;
+            case 'l':
+                expectingChar = '(';
+                break;
+            case '(':
+                expectingLeftDigit = true;
+                break;
+            case ',':
+                expectingLeftDigit = false;
+                inDigit = false;
+                expectingRightDigit = true;
+                break;
+            case ')':
+                var val = left * right;
+                Console.WriteLine($"Multiplying {left} and {right}");
+                expectingLeftDigit = false;
+                expectingRightDigit = false;
+                inDigit = false;
+                left = 0;
+                right = 0;
+                expectingChar = 'm';
+                return current + val;
+        }
+    } else if ((expectingLeftDigit || expectingRightDigit) && IsNumber(c))
+    {
+        var val =int.Parse(c.ToString());
+        if (expectingLeftDigit)
+        {
+            expectingChar = ',';
+
+            if (inDigit)
+            {
+                left = (left * 10) + val;
+            }
+            else
+            {
+                left = val;
+            }
+            
+            inDigit = true;
+        } else if (expectingRightDigit)
+        {
+            expectingChar = ')';
+            
+            if (inDigit)
+            {
+                right = (right * 10) + val;
+            }
+            else
+            {
+                right = val;
+            } 
+            inDigit = true;
+        }
+    }
+    else
+    {
+        
+        expectingLeftDigit = false;
+        expectingRightDigit = false;
+        inDigit = false;
+        left = 0;
+        right = 0;
+        expectingChar = 'm';
+    }
+
+    return current;
+});
+
+Console.WriteLine(count);
---
+++ Day3Part2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllText("input.txt");
+
+var matches = MulDoDontRegex().Matches(input);
+var sum = 0;
+var enabled = true;
+        
+foreach (Match match in matches)
+{
+    if (match.Groups[0].Value.StartsWith("mul("))
+        sum += enabled ? int.Parse(match.Groups[2].Value) * int.Parse(match.Groups[3].Value) : 0;
+    else
+        enabled = match.Groups[0].Value switch
+        {
+            "do()" => true,
+            "don't()" => false,
+            _ => enabled
+        };
+}
+        
+Console.WriteLine(sum);
+
+internal abstract partial class Program
+{
+    [GeneratedRegex(@"(mul\((-?\d+),(-?\d+)\)|do\(\)|don't\(\))")]
+    private static partial Regex MulDoDontRegex();
+}
